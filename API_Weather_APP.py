2113d24620b13874afcaeb326a1b51a80e06483e
SAKSHAM_CHOUHAN <131700345+samchouhan@users.noreply.github.com>
2026-04-05 05:14:36 +0000

    API_Weather_App

---
+++ API_Weather_APP.py
@@ -0,0 +1,29 @@
+#API weather app 
+#Install requests library if not already installed
+#Creat a API key from openweathermap.org and replace
+import requests 
+API_KEY ='692dd93b5d2157415f18ff5b3ffecd4f'
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+city = input('Enter city name: ')
+
+params = {
+    'q': city,
+    'appid': API_KEY,
+    'units': 'metric'
+}
+response = requests.get(BASE_URL, params=params)
+data = response.json()
+
+if data['cod'] == 200:
+    temprature = data['main']['temp']
+    description = data['weather'][0]['description']
+    weather=data['weather'][0]['description']
+    humidity = data['main']['humidity']
+    
+    print("\nWeather in {}:",city)
+    print("Temperature: {}°C".format(temprature))
+    print("condition:",weather)
+    print("Humidity: {}%".format(humidity))
+else:
+    print("City not found. Please check the city name and try again.")
+    
