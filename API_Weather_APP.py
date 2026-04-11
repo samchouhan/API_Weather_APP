16e1d81e0567326bedbbcd574232677ecf4d2245
SAKSHAM_CHOUHAN <131700345+samchouhan@users.noreply.github.com>
2026-04-11 10:32:07 +0530

    Enhance weather app with AQI data retrieval
    Updated API key placeholder and added AQI functionality.

--- API_Weather_APP.py
+++ API_Weather_APP.py
@@ -1,32 +1,65 @@
 #API weather app 
 #Install requests library if not already installed
 #Creat a API key from openweathermap.org and replace
-import requests 
-API_KEY ='692dd93b5d2157415f18ff5b3ffecd4f'
-BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+import requests
+
+API_KEY = 'your_api_key_here'
+
+WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
+AQI_URL = 'http://api.openweathermap.org/data/2.5/air_pollution'
+
 city = input('Enter city name: ')
 
+# Step 1: Weather API
 params = {
     'q': city,
     'appid': API_KEY,
     'units': 'metric'
 }
-response = requests.get(BASE_URL, params=params)
+
+response = requests.get(WEATHER_URL, params=params)
 data = response.json()
 
 if data['cod'] == 200:
-    temprature = data['main']['temp']
-    description = data['weather'][0]['description']
-    weather=data['weather'][0]['description']
+
+    # Weather Data
+    temperature = data['main']['temp']
+    weather = data['weather'][0]['description']
     humidity = data['main']['humidity']
     wind = data["wind"]["speed"]
 
-    
-    print("\nWeather in {}:",city)
-    print("Temperature: {}°C".format(temprature))
-    print("condition:",weather)
-    print("Humidity: {}%".format(humidity))
+    # Get Coordinates
+    lat = data['coord']['lat']
+    lon = data['coord']['lon']
+
+    # Step 2: AQI API
+    aqi_params = {
+        'lat': lat,
+        'lon': lon,
+        'appid': API_KEY
+    }
+
+    aqi_response = requests.get(AQI_URL, params=aqi_params)
+    aqi_data = aqi_response.json()
+
+    aqi = aqi_data['list'][0]['main']['aqi']
+
+    # AQI Meaning
+    aqi_status = {
+        1: "Good 🟢",
+        2: "Fair 🟡",
+        3: "Moderate 🟠",
+        4: "Poor 🔴",
+        5: "Very Poor 🟣"
+    }
+
+    print(f"\nWeather in {city}")
+    print("Temperature:", temperature, "°C")
+    print("Condition:", weather)
+    print("Humidity:", humidity, "%")
     print("Wind Speed:", wind, "m/s")
+
+    print("\nAir Quality Index:", aqi, "-", aqi_status.get(aqi, "Unknown"))
+
 else:
-    print("City not found. Please check the city name and try again.")
-    
+    print("City not found. Please check the city name.")
